e4f3191c0b93104c667a2d01f75fc45ca3d23cb6
victor <vili@nvidia.com>
2018-03-16 13:55:39 +0800

    cuda-8.0 is ready

--- devtools/sp_profile.sh
+++ devtools/sp_profile.sh
@@ -2,7 +2,7 @@ TOP=`pwd`/..
 exec_proc=$TOP/quda-build/tests/dslash_test
 cmd="$exec_proc --dslash-type clover"
 
-sp="/raid/tools/SP/SystemProfiler-linux-public-3.9.3747-52a8955/Target-x86_64/x86_64/sp"
+sp="/raid/tools/SP/SystemProfiler-linux-public-4.0.1163-b6fa40c/Target-x86_64/x86_64/sp"
 
 rm -fr ./*.qdstrm
-$sp profile --delay=2 --duration=10 -o quda_dslash_test.qdstrm -t cuda,cublas,curand,pthread,cudnn $cmd
+$sp profile --delay=2 --duration=10 -o quda_dslash_test.qdstrm -t cuda,cublas,curand,system,cudnn $cmd
